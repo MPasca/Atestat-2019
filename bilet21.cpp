6bb08f08e55d4b420b10dcd6b626452772cff969
Maria Pasca <32907490+MPasca@users.noreply.github.com>
2019-02-19 11:58:12 +0200

    Add files via upload

---
+++ bilet21.cpp
@@ -0,0 +1,22 @@
+#include<fstream>
+#include<bitset>
+using namespace std;
+fstream fin("date21.in",ios::in),fout("date21.out",ios::out);
+bitset<10>ap;
+int main(){
+    int n;
+    fin>>n;
+    while(n>0){
+        ap[n%10]=1;
+        n/=10;
+    }
+    for(int i=9;i>=0;i--){
+        if(ap[i]>0){
+            n=n*10+i;
+        }
+    }
+    fout<<n;
+    fin.close();
+    fout.close();
+    return 0;
+}
