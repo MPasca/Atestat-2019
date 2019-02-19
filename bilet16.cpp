67f198c39973bff8cbf96a8501e216206629b07b
Maria Pasca <32907490+MPasca@users.noreply.github.com>
2019-02-19 10:34:50 +0200

    Add files via upload

---
+++ bilet16.cpp
@@ -0,0 +1,35 @@
+#include<fstream>
+using namespace std;
+fstream fin("date16.in",ios::in),fout("date16.out",ios::out);
+int v[101],nr[101];
+int main(){
+    int n,x,ap=0,apmax=0,c;
+    fin>>n;
+    for(int i=1;i<=n;i++){
+        fin>>v[i];
+    }
+    for(int i=1;i<n;i++){
+        ap=1;
+        for(int j=i+1;j<=n;j++){
+            if(v[i]==v[j]){
+                ap++;
+            }
+        }
+        if(ap>apmax){
+            c=1;
+            apmax=ap;
+            nr[c]=v[i];
+        }
+        else if(ap==apmax){
+            c++;
+            nr[c]=v[i];
+        }
+        ap=0;
+    }
+    for(int i=1;i<=c;i++){
+        fout<<nr[i]<<" ";
+    }
+    fin.close();
+    fout.close();
+    return 0;
+}
