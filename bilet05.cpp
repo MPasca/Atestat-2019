d6ff96dc59367b0ec6aa7a1d26bca37e01d883c7
Maria Pasca <32907490+MPasca@users.noreply.github.com>
2019-02-20 19:48:17 +0200

    Update bilet05.cpp

--- bilet05.cpp
+++ bilet05.cpp
@@ -1,6 +1,6 @@
 #include<fstream>
 using namespace std;
-fstream fin("date5.in",ios::in),fout("date5.out",ios::out);
+fstream fin("date.in",ios::in),fout("date.out",ios::out);
 int sumcif(int a){
     int sum=0;
     if(a/10==0){
