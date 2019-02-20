d2bd99f9488bc321670318e2d502a99f5cf06f20
Maria Pasca <32907490+MPasca@users.noreply.github.com>
2019-02-20 19:28:16 +0200

    Add files via upload

---
+++ bilet25.cpp
@@ -0,0 +1,46 @@
+#include<fstream>
+#include<iostream>
+using namespace std;
+fstream fin("date25.in",ios::in),fout("date25.out",ios::out);
+int v[51];
+int main(){
+    int n,x,i,j,m;
+    fin>>n;
+    for(i=1;i<=n;i++){
+        fin>>v[i];
+    }
+    fin>>x;
+    i=1;
+    m=(i+n)/2;
+    do{
+        if(x==v[i]){
+            fout<<1;
+            fin.close();
+            fout.close();
+            return 0;
+        }
+        if(x==v[n]){
+            fout<<n;
+            fin.close();
+            fout.close();
+            return 0;
+        }
+        if(x>v[m]){
+            i=m;
+            m=(i+n)/2;
+        }else if(x<v[m]){
+            n=m;
+            m=(i+n)/2;
+        }
+        if(x==v[m]){
+            fout<<m;
+            fin.close();
+            fout.close();
+            return 0;
+        }
+    }while(i<n);
+    fout<<0;
+    fin.close();
+    fout.close();
+    return 0;
+}
