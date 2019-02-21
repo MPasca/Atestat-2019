ba852d1ba2bfe17bc205de47092a4702982c4e66
Maria Pasca <32907490+MPasca@users.noreply.github.com>
2019-02-21 12:32:11 +0200

    Update bilet20.cpp

--- bilet20.cpp
+++ bilet20.cpp
@@ -2,7 +2,7 @@
 #include<fstream>
 #include<cstring>
 using namespace std;
-fstream fin("date20.in",ios::in),fout("date20.out",ios::out);
+fstream fin("date.in",ios::in),fout("date.out",ios::out);
 char cuv[25],aux[25];
 int main(){
     fin>>cuv;
