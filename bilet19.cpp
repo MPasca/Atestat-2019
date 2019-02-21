9ed2719bf8ac1815af1b687058f6c4099de98a53
Maria Pasca <32907490+MPasca@users.noreply.github.com>
2019-02-21 12:31:49 +0200

    Update bilet19.cpp

--- bilet19.cpp
+++ bilet19.cpp
@@ -1,6 +1,6 @@
 #include<fstream>
 using namespace std;
-fstream fin("date19.in",ios::in),fout("date19.out",ios::out);
+fstream fin("date.in",ios::in),fout("date.out",ios::out);
 int x[21][21];
 int main(){
     int n,s=0;
