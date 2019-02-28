bfa3a07a5685cd63226951bc63ddd6047e798ab9
Maria Pasca <32907490+MPasca@users.noreply.github.com>
2019-02-28 12:52:34 +0200

    Add files via upload

---
+++ bilet02.sql
@@ -0,0 +1,80 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 28 Feb 2019 la 11:52
+-- Versiune server: 10.1.9-MariaDB
+-- PHP Version: 5.5.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bilet 2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structura de tabel pentru tabelul `abonati`
+--
+
+CREATE TABLE `abonati` (
+  `id` int(11) NOT NULL,
+  `nume` text NOT NULL,
+  `prenume` text NOT NULL,
+  `data_nasterii` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structura de tabel pentru tabelul `interventii`
+--
+
+CREATE TABLE `interventii` (
+  `idi` int(11) NOT NULL,
+  `tip_interventie` text NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `abonati`
+--
+ALTER TABLE `abonati`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `interventii`
+--
+ALTER TABLE `interventii`
+  ADD PRIMARY KEY (`idi`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `abonati`
+--
+ALTER TABLE `abonati`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `interventii`
+--
+ALTER TABLE `interventii`
+  MODIFY `idi` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
